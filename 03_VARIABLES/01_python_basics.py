d6ad6fe3db41b86571a3f69b55d86a3dbdbb27c3
manas-git-94 <manasmauryasik023@gmail.com>
2026-03-23 19:32:02 +0530

    Add files via upload

---
+++ 03_VARIABLES/01_python_basics.py
@@ -0,0 +1,3 @@
+age = 34 # integer
+    name = "Harry" # string
+    cgpa = 4.55 # float
---
+++ 03_VARIABLES/02_data_types.py
@@ -0,0 +1,16 @@
+
+age = 34
+print(age)
+print(type(age))
+
+cgpa = 8.2 
+print(cgpa)
+print(type(cgpa))
+
+name = "harry"
+print (name)
+print(type(name))
+
+is_completed = True # can also be False
+print(is_completed)
+print(type(is_completed))
